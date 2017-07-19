2b69c32315d39f319a6b966dc502ce52f138cccd
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-07-20 00:04:48 +0200

    Added package for managing storage

---
+++ storage/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+# Project       Goffrey
+# Description   A simple IPAM
+# License       GPL version 2 (see GPL.txt for details)
+
+__all__ = []
