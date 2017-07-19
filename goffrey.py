0191cc63561556049e84a9ab2147792636a1690b
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-07-19 18:46:27 +0200

    Added comment

--- goffrey.py
+++ goffrey.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+# Project       Goffrey
+# Description   A simple IPAM
+# License       GPL version 2 (see GPL.txt for details)
+
 import argparse
 
 import sys
