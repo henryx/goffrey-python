b9c2adc2e6a4938581a6bb7f1bfe94d6d1608e56
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-07-19 17:36:18 +0200

    Started implementation

---
+++ goffrey.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+__author__ = "Enrico Bianchi"
+__copyright__ = "Copyright 2017, Enrico Bianchi"
+__credits__ = ["Enrico Bianchi", ]
+__license__ = "GPLv2"
+__maintainer__ = "Enrico Bianchi"
+__email__ = "enrico.bianchi@gmail.com"
+__status__ = "Development"
+__version__ = "0.0.0"
+
+
+def main():
+    print("Goffrey version {}".format(__version__))
+
+
+if __name__ == "__main__":
+    main()
