2b70d07159a7f6a87dc4f0af8d6e4ba3e5fee6b3
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-07-19 17:43:42 +0200

    Started argument parser

--- goffrey.py
+++ goffrey.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import argparse
+
+import sys
+
 __author__ = "Enrico Bianchi"
 __copyright__ = "Copyright 2017, Enrico Bianchi"
 __credits__ = ["Enrico Bianchi", ]
@@ -10,7 +14,15 @@ __status__ = "Development"
 __version__ = "0.0.0"
 
 
+def init_args():
+    args = argparse.ArgumentParser(description="Goffrey")
+
+    return args
+
+
 def main():
+    args = init_args().parse_args(sys.argv)
+
     print("Goffrey version {}".format(__version__))
 
 
