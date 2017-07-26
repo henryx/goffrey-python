002b16dbcaf33c3977697cedab87e47f56861692
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-07-26 13:25:46 +0200

    Added package for executing operations

---
+++ operations/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+# Project       Goffrey
+# Description   A simple IPAM
+# License       GPL version 2 (see GPL.txt for details)
+import configparser
+
+_author__ = "Enrico Bianchi"
+__copyright__ = "Copyright 2017, Enrico Bianchi"
+__credits__ = ["Enrico Bianchi", ]
+__license__ = "GPLv2"
+__maintainer__ = "Enrico Bianchi"
+__email__ = "enrico.bianchi@gmail.com"
+__status__ = "Development"
+__version__ = "0.0.0"
+
+__all__ = []
+
+
+class Operation:
+    _cfg = None
+
+    def __init__(self, cfg):
+        self._cfg = configparser.ConfigParser()
+        self._cfg.read(cfg)
+
+    def start(self, name, network, netmask):
+        raise NotImplementedError("Base method not implemented in this class")
