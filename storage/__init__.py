adac56ab55de438d2f37b1df8108ecb498bac547
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-07-26 12:52:59 +0200

    Added authorship informations

--- storage/__init__.py
+++ storage/__init__.py
@@ -3,4 +3,13 @@
 # Description   A simple IPAM
 # License       GPL version 2 (see GPL.txt for details)
 
+_author__ = "Enrico Bianchi"
+__copyright__ = "Copyright 2017, Enrico Bianchi"
+__credits__ = ["Enrico Bianchi", ]
+__license__ = "GPLv2"
+__maintainer__ = "Enrico Bianchi"
+__email__ = "enrico.bianchi@gmail.com"
+__status__ = "Development"
+__version__ = "0.0.0"
+
 __all__ = ["database"]
--- storage/database.py
+++ storage/database.py
@@ -3,6 +3,14 @@
 # Description   A simple IPAM
 # License       GPL version 2 (see GPL.txt for details)
 
+_author__ = "Enrico Bianchi"
+__copyright__ = "Copyright 2017, Enrico Bianchi"
+__credits__ = ["Enrico Bianchi", ]
+__license__ = "GPLv2"
+__maintainer__ = "Enrico Bianchi"
+__email__ = "enrico.bianchi@gmail.com"
+__status__ = "Development"
+__version__ = "0.0.0"
 
 class Database(object):
     _conn = None
